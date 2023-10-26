feed1cd668ed94bf9b28b949d748d195d376e643
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2023-10-26 08:58:08 +0530

    Added the drawer

---
+++ routes/screens/customDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CustomDrawer = () => {
+  return (
+    <View>
+      <Text>Hi there</Text>
+    </View>
+  );
+};
+
+export default CustomDrawer;
--- routes/screens/home.js
+++ routes/screens/home.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, Button, Modal } from 'react-native'
+import CustomDrawer from './customDrawer'; 
 
-const Home = () => {
+const HomeScreen = () => {
+  const [isDrawerVisible, setDrawerVisible] = useState(false);
+
   return (
     <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+      <Text>Your home screen content</Text>
+      <Button title="Open Drawer" onPress={() => setDrawerVisible(true)} />
 
-export default Home
+      <Modal
+        visible={isDrawerVisible}
+        animationType="slide"
+        transparent={true}
+      >
+        <CustomDrawer />
+        <Button title="Close Drawer" onPress={() => setDrawerVisible(false)} />
+      </Modal>
+    </View>
+  );
+};
+
+export default HomeScreen;
